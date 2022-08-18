976cc1d75ebc0ff60db27d21b7658040cd7079e2
ibrahim <ibrahimwali4u@gmail.com>
2022-08-18 15:40:50 +0100

    add text course

--- resources/views/admin/webinars/create.blade.php
+++ resources/views/admin/webinars/create.blade.php
@@ -71,7 +71,7 @@
                                                 <select name="type" class="custom-select @error('type')  is-invalid @enderror">
                                                     <option value="webinar" @if((!empty($webinar) and $webinar->isWebinar()) or old('type') == \App\Models\Webinar::$webinar) selected @endif>{{ trans('webinars.webinar') }}</option>
                                                     <option value="course" @if((!empty($webinar) and $webinar->isCourse()) or old('type') == \App\Models\Webinar::$course) selected @endif>{{ trans('product.video_course') }}</option>
-                                                    <option>{{ trans('product.text_course') }} (Paid plugin)</option>
+                                                    <option value="text_lesson" @if((!empty($webinar) and $webinar->isTextCourse()) or old('type') == \App\Models\Webinar::$textLesson) selected @endif>{{ trans('product.text_course') }}</option>
                                                 </select>
 
                                                 @error('type')
