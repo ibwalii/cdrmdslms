49f5b311313281ac0fe32c0f37d183009ae8aa72
Piccolo <kabiryusufbashir@gmail.com>
2022-09-08 18:08:15 +0100

    Fixed step 6

--- resources/views/web/default/panel/webinar/create_includes/step_6.blade.php
+++ resources/views/web/default/panel/webinar/create_includes/step_6.blade.php
@@ -1,30 +1,29 @@
 @push('styles_top')
-    <link href="/assets/default/vendors/sortable/jquery-ui.min.css"/>
+
 @endpush
 
 
 <section class="mt-50">
     <div class="">
-        <h2 class="section-title after-line">{{ trans('public.faq') }} ({{ trans('public.optional') }})</h2>
+        {{-- <h2 class="section-title after-line">{{ trans('public.quiz_certificate') }} ({{ trans('public.optional') }})</h2> --}}
+        <h2 class="section-title after-line">Quiz ({{ trans('public.optional') }})</h2>
     </div>
 
-    <button id="webinarAddFAQ" data-webinar-id="{{ $webinar->id }}" type="button" class="btn btn-primary btn-sm mt-15">{{ trans('public.add_faq') }}</button>
+    <button id="webinarAddQuiz" data-webinar-id="{{ $webinar->id }}" type="button" class="btn btn-primary btn-sm mt-15">{{ trans('public.add_quiz') }}</button>
 
     <div class="row mt-10">
         <div class="col-12">
 
-            <div class="accordion-content-wrapper mt-15" id="faqsAccordion" role="tablist" aria-multiselectable="true">
-                @if(!empty($webinar->faqs) and count($webinar->faqs))
-                    <ul class="draggable-lists" data-order-table="faqs">
-                        @foreach($webinar->faqs as $faqInfo)
-                            @include('web.default.panel.webinar.create_includes.accordions.faq',['webinar' => $webinar,'faq' => $faqInfo])
-                        @endforeach
-                    </ul>
+            <div class="accordion-content-wrapper mt-15" id="quizzesAccordion" role="tablist" aria-multiselectable="true">
+                @if(!empty($webinar->quizzes) and count($webinar->quizzes))
+                    @foreach($webinar->quizzes as $quizInfo)
+                        @include('web.default.panel.webinar.create_includes.accordions.quiz',['webinar' => $webinar,'quizInfo' => $quizInfo])
+                    @endforeach
                 @else
                     @include(getTemplate() . '.includes.no-result',[
-                        'file_name' => 'faq.png',
-                        'title' => trans('public.faq_no_result'),
-                        'hint' => trans('public.faq_no_result_hint'),
+                        'file_name' => 'cert.png',
+                        'title' => trans('public.quizzes_no_result'),
+                        'hint' => trans('public.quizzes_no_result_hint'),
                     ])
                 @endif
             </div>
@@ -32,11 +31,16 @@
     </div>
 </section>
 
-<div id="newFaqForm" class="d-none">
-    @include('web.default.panel.webinar.create_includes.accordions.faq',['webinar' => $webinar])
+<div id="newQuizForm" class="d-none">
+    @include('web.default.panel.webinar.create_includes.accordions.quiz',['webinar' => $webinar,'quizInfo' => null])
 </div>
 
 
 @push('scripts_bottom')
-    <script src="/assets/default/vendors/sortable/jquery-ui.min.js"></script>
+    <script>
+        var saveSuccessLang = '{{ trans('webinars.success_store') }}';
+        var quizzesSectionLang = '{{ trans('quiz.quizzes_section') }}';
+    </script>
+
+    <script src="/assets/default/js/panel/quiz.min.js"></script>
 @endpush
