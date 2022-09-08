d66f0b02eb223621a9942b9d9815639247ead445
Piccolo <kabiryusufbashir@gmail.com>
2022-09-08 17:50:21 +0100

    Remove Step 3 Entirely

--- resources/views/web/default/panel/webinar/create_includes/step_3.blade.php
+++ resources/views/web/default/panel/webinar/create_includes/step_3.blade.php
@@ -1,76 +1,115 @@
 @push('styles_top')
+    <link rel="stylesheet" href="/assets/default/vendors/select2/select2.min.css">
     <link rel="stylesheet" href="/assets/default/vendors/daterangepicker/daterangepicker.min.css">
+    <link rel="stylesheet" href="/assets/vendors/summernote/summernote-bs4.min.css">
     <link href="/assets/default/vendors/sortable/jquery-ui.min.css"/>
 @endpush
 
-<div class="row">
-    <div class="col-12 col-md-6">
-        {{-- <div class="form-group mt-30 d-flex align-items-center justify-content-between mb-5">
-            <label class="cursor-pointer input-label" for="subscribeSwitch">{{ trans('webinars.include_subscribe') }}</label>
-            <div class="custom-control custom-switch">
-                <input type="checkbox" name="subscribe" class="custom-control-input" id="subscribeSwitch" {{ !empty($webinar) && $webinar->subscribe ? 'checked' : (old('subscribe') ? 'checked' : '')  }}>
-                <label class="custom-control-label" for="subscribeSwitch"></label>
-            </div>
-        </div> --}}
-
-        {{-- <div>
-            <p class="font-12 text-gray">- {{ trans('forms.subscribe_hint') }}</p>
-        </div> --}}
-
-        <div class="form-group mt-15">
-            {{-- <label class="input-label">{{ trans('public.price') }}</label> --}}
-            <input type="hidden" name="price" value="0" class="form-control @error('price')  is-invalid @enderror" placeholder="{{ trans('public.0_for_free') }}"/>
-            {{-- <input type="number" name="price" value="{{ !empty($webinar) ? $webinar->price : old('price') }}" class="form-control @error('price')  is-invalid @enderror" placeholder="{{ trans('public.0_for_free') }}"/> --}}
-            @error('price')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
+@if($webinar->isWebinar())
+    <section class="mt-50">
+        <div class="">
+            <h2 class="section-title after-line">{{ trans('public.sessions') }} ({{ trans('public.optional') }})</h2>
         </div>
-    </div>
-</div>
 
-<section class="mt-30">
+        <button type="button" class="js-add-chapter btn btn-primary btn-sm mt-15" data-webinar-id="{{ $webinar->id }}" data-type="{{ \App\Models\WebinarChapter::$chapterSession }}">{{ trans('public.new_chapter') }}</button>
+
+        @include('web.default.panel.webinar.create_includes.accordions.chapter',
+                    [
+                        'chapters' => $webinar->chapters ,
+                        'type' => \App\Models\WebinarChapter::$chapterSession,
+                        'relationMethod' => 'sessions',
+                        'includeFileName' => 'session',
+                        'tableName' => 'sessions',
+                        'variableName' => 'session',
+                        'emptyState' => [
+                            'file_name' => 'meet.png',
+                            'title' => trans('public.sessions_no_result'),
+                            'hint' => trans('public.sessions_no_result_hint'),
+                        ]
+                    ]
+                )
+    </section>
+
+    <div id="newSessionForm" class="d-none">
+        @include('web.default.panel.webinar.create_includes.accordions.session',['webinar' => $webinar])
+    </div>
+@endif
+
+<section class="mt-50">
     <div class="">
-        {{-- <h2 class="section-title after-line">{{ trans('webinars.sale_plans') }} ({{ trans('public.optional') }})</h2> --}}
-
-
-        {{-- <div class="mt-15">
-            <p class="font-12 text-gray">- {{ trans('webinars.sale_plans_hint_1') }}</p>
-            <p class="font-12 text-gray">- {{ trans('webinars.sale_plans_hint_2') }}</p>
-            <p class="font-12 text-gray">- {{ trans('webinars.sale_plans_hint_3') }}</p>
-        </div> --}}
+        <h2 class="section-title after-line">{{ trans('public.files') }} ({{ trans('public.optional') }})</h2>
     </div>
-
-    {{-- <button id="webinarAddTicket" data-webinar-id="{{ $webinar->id }}" type="button" class="btn btn-primary btn-sm mt-15">{{ trans('public.add_plan') }}</button> --}}
-
-    <div class="row mt-10">
-        <div class="col-12">
-
-            {{-- <div class="accordion-content-wrapper mt-15" id="ticketsAccordion" role="tablist" aria-multiselectable="true">
-                @if(!empty($webinar->tickets) and count($webinar->tickets))
-                    <ul class="draggable-lists" data-order-table="tickets">
-                        @foreach($webinar->tickets as $ticketInfo)
-                            @include('web.default.panel.webinar.create_includes.accordions.ticket',['webinar' => $webinar,'ticket' => $ticketInfo])
-                        @endforeach
-                    </ul>
-                @else
-                    @include(getTemplate() . '.includes.no-result',[
-                        'file_name' => 'ticket.png',
-                        'title' => trans('public.ticket_no_result'),
-                        'hint' => trans('public.ticket_no_result_hint'),
-                    ])
-                @endif
-            </div> --}}
-        </div>
+    <div class="mt-15">
+        <p class="font-12 text-gray">- {{ trans('webinars.files_hint_1') }}</p>
+        <p class="font-12 text-gray">- {{ trans('webinars.files_hint_2') }}</p>
     </div>
+    <button type="button" class="js-add-chapter btn btn-primary btn-sm mt-15" data-webinar-id="{{ $webinar->id }}" data-type="{{ \App\Models\WebinarChapter::$chapterFile }}">{{ trans('public.new_chapter') }}</button>
+
+    @include('web.default.panel.webinar.create_includes.accordions.chapter',
+                [
+                    'chapters' => $webinar->chapters ,
+                    'type' => \App\Models\WebinarChapter::$chapterFile,
+                    'relationMethod' => 'files',
+                    'includeFileName' => 'file',
+                    'tableName' => 'files',
+                    'variableName' => 'file',
+                    'emptyState' => [
+                        'file_name' => 'files.png',
+                        'title' => trans('public.files_no_result'),
+                        'hint' => trans('public.files_no_result_hint'),
+                    ]
+                ]
+            )
 </section>
 
-<div id="newTicketForm" class="d-none">
-    @include('web.default.panel.webinar.create_includes.accordions.ticket',['webinar' => $webinar])
+<div id="newFileForm" class="d-none">
+    @include('web.default.panel.webinar.create_includes.accordions.file',['webinar' => $webinar])
 </div>
 
+<div id="newInteractiveFileForm" class="d-none">
+    @include('web.default.panel.webinar.create_includes.accordions.new_interactive_file',['webinar' => $webinar])
+</div>
+
+@if($webinar->isTextCourse())
+    <section class="mt-50">
+        <div class="">
+            <h2 class="section-title after-line">{{ trans('public.test_lesson') }} ({{ trans('public.optional') }})</h2>
+        </div>
+
+        <button type="button" class="js-add-chapter btn btn-primary btn-sm mt-15" data-webinar-id="{{ $webinar->id }}" data-type="{{ \App\Models\WebinarChapter::$chapterTextLesson }}">{{ trans('public.new_chapter') }}</button>
+
+        @include('web.default.panel.webinar.create_includes.accordions.chapter',
+                    [
+                        'chapters' => $webinar->chapters ,
+                        'type' => \App\Models\WebinarChapter::$chapterTextLesson,
+                        'relationMethod' => 'textLessons',
+                        'includeFileName' => 'text-lesson',
+                        'tableName' => 'text_lessons',
+                        'variableName' => 'textLesson',
+                        'emptyState' => [
+                            'file_name' => 'files.png',
+                            'title' => trans('public.text_lesson_no_result'),
+                            'hint' => trans('public.text_lesson_no_result_hint'),
+                        ]
+                    ]
+                )
+    </section>
+
+    <div id="newTextLessonForm" class="d-none">
+        @include('web.default.panel.webinar.create_includes.accordions.text-lesson',['webinar' => $webinar])
+    </div>
+@endif
+
+@include('web.default.panel.webinar.create_includes.chapter_modal')
+
 @push('scripts_bottom')
+    <script src="/assets/default/vendors/select2/select2.min.js"></script>
     <script src="/assets/default/vendors/daterangepicker/daterangepicker.min.js"></script>
+    <script src="/assets/vendors/summernote/summernote-bs4.min.js"></script>
     <script src="/assets/default/vendors/sortable/jquery-ui.min.js"></script>
+
+    <script>
+        var requestFailedLang = '{{ trans('public.request_failed') }}';
+        var thisLiveHasEndedLang = '{{ trans('update.this_live_has_been_ended') }}';
+    </script>
 @endpush
