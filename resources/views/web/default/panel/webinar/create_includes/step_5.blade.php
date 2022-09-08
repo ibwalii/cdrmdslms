bbfa3c1b1be11877759510f7ad1a65c3d095cca8
Piccolo <kabiryusufbashir@gmail.com>
2022-09-08 18:06:45 +0100

    Fixed step 5

--- resources/views/web/default/panel/webinar/create_includes/step_5.blade.php
+++ resources/views/web/default/panel/webinar/create_includes/step_5.blade.php
@@ -1,31 +1,30 @@
 @push('styles_top')
-    <link rel="stylesheet" href="/assets/default/vendors/select2/select2.min.css">
     <link href="/assets/default/vendors/sortable/jquery-ui.min.css"/>
 @endpush
 
 
 <section class="mt-50">
     <div class="">
-        <h2 class="section-title after-line">{{ trans('public.prerequisites') }} ({{ trans('public.optional') }})</h2>
+        <h2 class="section-title after-line">{{ trans('public.faq') }} ({{ trans('public.optional') }})</h2>
     </div>
 
-    <button id="webinarAddPrerequisites" data-webinar-id="{{ $webinar->id }}" type="button" class="btn btn-primary btn-sm mt-15">{{ trans('public.add_prerequisites') }}</button>
+    <button id="webinarAddFAQ" data-webinar-id="{{ $webinar->id }}" type="button" class="btn btn-primary btn-sm mt-15">{{ trans('public.add_faq') }}</button>
 
     <div class="row mt-10">
         <div class="col-12">
 
-            <div class="accordion-content-wrapper mt-15" id="prerequisitesAccordion" role="tablist" aria-multiselectable="true">
-                @if(!empty($webinar->prerequisites) and count($webinar->prerequisites))
-                    <ul class="draggable-lists" data-order-table="prerequisites">
-                        @foreach($webinar->prerequisites as $prerequisiteInfo)
-                            @include('web.default.panel.webinar.create_includes.accordions.prerequisites',['webinar' => $webinar,'prerequisite' => $prerequisiteInfo])
+            <div class="accordion-content-wrapper mt-15" id="faqsAccordion" role="tablist" aria-multiselectable="true">
+                @if(!empty($webinar->faqs) and count($webinar->faqs))
+                    <ul class="draggable-lists" data-order-table="faqs">
+                        @foreach($webinar->faqs as $faqInfo)
+                            @include('web.default.panel.webinar.create_includes.accordions.faq',['webinar' => $webinar,'faq' => $faqInfo])
                         @endforeach
                     </ul>
                 @else
                     @include(getTemplate() . '.includes.no-result',[
-                        'file_name' => 'comment.png',
-                        'title' => trans('public.prerequisites_no_result'),
-                        'hint' => trans('public.prerequisites_no_result_hint'),
+                        'file_name' => 'faq.png',
+                        'title' => trans('public.faq_no_result'),
+                        'hint' => trans('public.faq_no_result_hint'),
                     ])
                 @endif
             </div>
@@ -33,12 +32,11 @@
     </div>
 </section>
 
-<div id="newPrerequisiteForm" class="d-none">
-    @include('web.default.panel.webinar.create_includes.accordions.prerequisites',['webinar' => $webinar])
+<div id="newFaqForm" class="d-none">
+    @include('web.default.panel.webinar.create_includes.accordions.faq',['webinar' => $webinar])
 </div>
 
 
 @push('scripts_bottom')
-    <script src="/assets/default/vendors/select2/select2.min.js"></script>
     <script src="/assets/default/vendors/sortable/jquery-ui.min.js"></script>
 @endpush
