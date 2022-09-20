111128525c8424ebe4e1621056eb266d08fc82b5
ibrahim <ibrahimwali4u@gmail.com>
2022-09-20 07:13:03 +0100

    Quiz added

--- resources/views/web/default/panel/quizzes/create_quiz_form.blade.php
+++ resources/views/web/default/panel/quizzes/create_quiz_form.blade.php
@@ -30,7 +30,9 @@
                         <div class="form-group mt-25">
                             <label class="input-label">{{ trans('panel.webinar') }}</label>
                             <select name="ajax[webinar_id]" class="js-ajax-webinar_id custom-select">
-                            <option>Paid Plugin</option>
+                                @foreach($webinars as $webinar)
+                                    <option value="{{ $webinar->id }}" {{  (!empty($quiz) and $quiz->webinar_id == $webinar->id) ? 'selected' : '' }}>{{ $webinar->title }}</option>
+                                @endforeach
                             </select>
                         </div>
                     @else
